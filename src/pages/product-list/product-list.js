7ebc4910417127117e341bfe775471d171370aab
jiyou228 <jhjy504@naver.com>
2025-11-12 16:34:45 +0900

    [feat] 제품 리스트 UI 구현 - 반응형

--- src/pages/product-list/product-list.js
+++ src/pages/product-list/product-list.js
@@ -11,4 +11,33 @@ if (sortBtn && sortMenu) {
         }
     });
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const hiddenFilterBtn = document.getElementById("hidden-filter");
+    const desktopSidebar = document.getElementById("desktop-sidebar");
+    hiddenFilterBtn?.addEventListener("click", () => {
+        if (desktopSidebar?.classList.contains("lg:flex")) {
+            desktopSidebar.classList.remove("lg:flex");
+        }
+        else {
+            desktopSidebar?.classList.add("lg:flex");
+        }
+    });
+});
+const imgOn = "/src/assets/img/Property 1=On.svg";
+const imgOff = "/src/assets/img/Property 1=Off.png";
+const checkboxImgs = document.querySelectorAll(".checkbox-img");
+checkboxImgs.forEach((img) => {
+    const targetId = img.getAttribute("data-target");
+    const checkbox = document.getElementById(targetId);
+    // 클릭 시 상태 변경
+    img.addEventListener("click", () => {
+        checkbox.checked = !checkbox.checked;
+        updateCheckboxImage(checkbox, img);
+    });
+    // 체크박스 직접 눌렀을 때도 이미지 갱신
+    checkbox.addEventListener("change", () => updateCheckboxImage(checkbox, img));
+});
+function updateCheckboxImage(checkbox, img) {
+    img.src = checkbox.checked ? imgOn : imgOff;
+}
 export {};
--- src/pages/product-list/product-list.ts
+++ src/pages/product-list/product-list.ts
@@ -14,3 +14,44 @@ if (sortBtn && sortMenu) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hiddenFilterBtn = document.getElementById("hidden-filter");
+  const desktopSidebar = document.getElementById("desktop-sidebar");
+
+  hiddenFilterBtn?.addEventListener("click", () => {
+    if (desktopSidebar?.classList.contains("lg:flex")) {
+      desktopSidebar.classList.remove("lg:flex");
+    } else {
+      desktopSidebar?.classList.add("lg:flex");
+    }
+  });
+});
+
+const imgOn = "/src/assets/img/Property 1=On.svg";
+const imgOff = "/src/assets/img/Property 1=Off.png";
+
+const checkboxImgs = document.querySelectorAll(".checkbox-img");
+
+checkboxImgs.forEach((img) => {
+  const targetId = img.getAttribute("data-target");
+  const checkbox = document.getElementById(targetId!) as HTMLInputElement;
+
+  // 클릭 시 상태 변경
+  img.addEventListener("click", () => {
+    checkbox.checked = !checkbox.checked;
+    updateCheckboxImage(checkbox, img as HTMLImageElement);
+  });
+
+  // 체크박스 직접 눌렀을 때도 이미지 갱신
+  checkbox.addEventListener("change", () =>
+    updateCheckboxImage(checkbox, img as HTMLImageElement)
+  );
+});
+
+function updateCheckboxImage(
+  checkbox: HTMLInputElement,
+  img: HTMLImageElement
+) {
+  img.src = checkbox.checked ? imgOn : imgOff;
+}
